b73fd306e2adf0c29c629903adf1676903c9e170
Rutvi11shah <147698958+Rutvi11shah@users.noreply.github.com>
2023-10-12 10:10:34 +0200

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,85 @@
+#Exercise 1
+print("AWS Re/Start is an amazing program!")
+print("Print Hi, I am excited, happy, and overwhelmed.")
+
+#Exercise 2
+name = "Rutvi"
+age = 27
+output = name + ", is " + str(age) + " years old."
+print(output)
+
+#Exercise 3
+variable =7
+variable +=20
+print(variable)
+
+#Exercise 4
+glass_of_water = 10
+print("I drank " + str(glass_of_water) + " glasses of water yesterday.")
+
+#Exercise 5
+int_var = 27
+float_var = 1.43
+complex_var = 7+29j
+bool_var = True
+print("int_var: {int_var} ({type(int_var)}), float_var: {float_var} ({type(float_var)}), complex_var: {complex_var} ({type(complex_var)}), bool_var: {bool_var} ({type(bool_var)})")
+
+#Exercise 6
+String_1 = "Rutvi"
+String_2 = "Shah"
+String = String_1+ String_2
+print(String)
+
+#Exercise 7
+num1 = int(input("Enter 1st number"))
+num2 = int(input("Enter 2nd number"))
+num1 = float(num1)
+num2 = float(num2)
+add = num1+num2
+print("The sum of {} and {} is{}".format(num1,num2,add))
+
+#Exercise 8
+def greet():
+    print("Programming is amazing!")
+greet()
+def greet():
+    print("Hello, I feel curious, inspired and amazed.")
+greet()
+
+#Exercise 9
+favourite_color = input("Enter your favourite color : ")
+birth_month = input("Enter oyur birth month : ")
+output_string= ("{} , is the color of the month of {}".format(favourite_color, birth_month ) )
+print(output_string)
+
+#Exercise 10
+my_variable = 13
+subtract_variable = 6
+my_variable -=subtract_variable
+print("updated value of the variable is ", my_variable)
+
+#Exercise 11
+books_read = input("Enter number of book you read this month")
+print("I have read {} books this month.".format(books_read))
+
+#Exercise 12
+my_string = "Hello, World!"
+my_tuple = (1, 2, 3, 4, 5)
+my_list = [10, 20, 30, 40, 50]
+my_dict = {'name': 'Rutvi', 'age': 27, 'city': 'Norway'}
+
+print(f"String: {my_string}, Type: {type(my_string)}")
+print(f"Tuple: {my_tuple}, Type: {type(my_tuple)}")
+print(f"List: {my_list}, Type: {type(my_list)}")
+print(f"Dictionary: {my_dict}, Type: {type(my_dict)}")
+
+#Exercise 13
+Phrase_1 = "Learning Pyhton"
+Phrase_2 = "is interesting."
+concatenate_Phrase = Phrase_1+Phrase_2
+print(concatenate_Phrase)
+
+#Exercise 14
+
+
+#Exercise 15
